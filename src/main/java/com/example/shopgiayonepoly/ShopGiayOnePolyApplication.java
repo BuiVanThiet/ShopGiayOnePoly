182c682c766a1fc592ff52dee5d75e8e80cfcf30
Bui Van Thiet <thietbuivan2512@gmail.com>
2024-09-02 05:50:12 +0700

    ShopGiayOnePoly

---
+++ src/main/java/com/example/shopgiayonepoly/ShopGiayOnePolyApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopgiayonepoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopGiayOnePolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopGiayOnePolyApplication.class, args);
+	}
+
+}
