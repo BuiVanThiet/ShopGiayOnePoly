2bf40838e422d8d9f3c040d86f159aa180c5af36
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-07 00:01:04 +0700

    m

--- src/main/java/com/example/shopgiayonepoly/service/StaffService.java
+++ src/main/java/com/example/shopgiayonepoly/service/StaffService.java
@@ -1,28 +1,28 @@
-package com.example.shopgiayonepoly.service;
-
-import com.example.shopgiayonepoly.entites.Staff;
-import com.example.shopgiayonepoly.repositores.StaffRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class StaffService implements UserDetailsService {
-    @Autowired
-    StaffRepository staffRepository;
-    @Override
-    public UserDetails loadUserByUsername(String acount) throws UsernameNotFoundException {
-        Staff staff = staffRepository.findByAcount(acount);
-        if(staff != null){
-            var springStaff = User.withUsername(staff.getAcount())
-                    .password(staff.getPassword())
-                    .roles(staff.getRole().getNameRole())
-                    .build();
-            return springStaff;
-        }
-        return null;
-    }
-}
+//package com.example.shopgiayonepoly.service;
+//
+//import com.example.shopgiayonepoly.entites.Staff;
+//import com.example.shopgiayonepoly.repositores.StaffRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class StaffService implements UserDetailsService {
+//    @Autowired
+//    StaffRepository staffRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String acount) throws UsernameNotFoundException {
+//        Staff staff = staffRepository.findByAcount(acount);
+//        if(staff != null){
+//            var springStaff = User.withUsername(staff.getAcount())
+//                    .password(staff.getPassword())
+//                    .roles(staff.getRole().getNameRole())
+//                    .build();
+//            return springStaff;
+//        }
+//        return null;
+//    }
+//}
