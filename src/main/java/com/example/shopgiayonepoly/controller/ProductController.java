612e733a1469ea79527dea4e5c76ca2a894a96b3
quanggvinh <vinhdaik2004@gmail.com>
2024-09-06 23:03:05 +0700

    0609,23h03

--- src/main/java/com/example/shopgiayonepoly/controller/ProductController.java
+++ src/main/java/com/example/shopgiayonepoly/controller/ProductController.java
@@ -8,8 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+    String mess = "";
+    String colorMess = "";
     @GetMapping("/")
-    public String a() {
+    public String a(ModelMap modelMap) {
+        modelMap.addAttribute("message",mess);
+        modelMap.addAttribute("check",colorMess);
+        this.mess = "";
+        this.colorMess = "";
+        modelMap.addAttribute("page","/Product/list");
         return "Product/list";
+    }
+
+    @GetMapping("/create")
+    public String create(ModelMap modelMap) {
+        modelMap.addAttribute("message",mess);
+        modelMap.addAttribute("check",colorMess);
+        this.mess = "";
+        this.colorMess = "";
+        modelMap.addAttribute("page","/Product/create");
+        return "Product/create";
+    }
+
+    @GetMapping("/attribute/color")
+    public String color(ModelMap modelMap) {
+//        modelMap.addAttribute("message",mess);
+//        modelMap.addAttribute("check",colorMess);
+//        this.mess = "";
+//        this.colorMess = "";
+        modelMap.addAttribute("page","/Attribute/color");
+        return "Attribute/color";
     }
 }
