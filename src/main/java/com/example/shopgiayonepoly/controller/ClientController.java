36879e37c17cd8a6177d3144afb405fb39e92420
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-08 21:26:02 +0700

    push,0809,

--- src/main/java/com/example/shopgiayonepoly/controller/ClientController.java
+++ src/main/java/com/example/shopgiayonepoly/controller/ClientController.java
@@ -3,20 +3,15 @@ package com.example.shopgiayonepoly.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/onepoly")
 public class ClientController {
-    @GetMapping("/base")
-    public String getFormHomeClientBase(){
-        return "client/base";
-    }
     @GetMapping("/home")
     public String getFormHomeClient(){
         return "client/homepage";
     }
-    @GetMapping("/address")
-    public String getCallAPIGHN(){
-        return "client/address";
+    @GetMapping("/products")
+    public String getFormProduct(){
+        return "client/product";
     }
 }
---
+++ src/main/java/com/example/shopgiayonepoly/implement/ClientServiceImplement.java
@@ -0,0 +1,8 @@
+package com.example.shopgiayonepoly.implement;
+
+import com.example.shopgiayonepoly.service.ClientService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImplement implements ClientService {
+}
