b276ee8c5a3c5eaab5c2357e56717719e888dc92
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-11 21:10:13 +0700

    push,1109,21h10,TuanAnh

--- src/main/java/com/example/shopgiayonepoly/repositores/StaffSecurityRepository.java
+++ src/main/java/com/example/shopgiayonepoly/repositores/StaffSecurityRepository.java
@@ -1,10 +1,10 @@
-//package com.example.shopgiayonepoly.repositores;
-//
-//import com.example.shopgiayonepoly.entites.Staff;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StaffSecurityRepository extends JpaRepository<Staff, Integer> {
-//    public Staff findByAcountOrEmail(String acount, String email);
-//}
+package com.example.shopgiayonepoly.repositores;
+
+import com.example.shopgiayonepoly.entites.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffSecurityRepository extends JpaRepository<Staff, Integer> {
+    public Staff findByAcountOrEmail(String acount, String email);
+}
