b15a957683d421734c946d2b63baedb54328f790
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-18 21:29:10 +0700

    push,1809,21h29,TuanAnh

--- src/main/java/com/example/shopgiayonepoly/implement/StaffSecuritiImplement.java
+++ src/main/java/com/example/shopgiayonepoly/implement/StaffSecuritiImplement.java
@@ -1,36 +1,36 @@
-//package com.example.shopgiayonepoly.implement;
-//
-//import com.example.shopgiayonepoly.entites.Staff;
-//import com.example.shopgiayonepoly.repositores.StaffSecurityRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class StaffSecuritiImplement implements UserDetailsService {
-//    @Autowired
-//    StaffSecurityRepository staffRepository;
-//    @Override
-//    public UserDetails loadUserByUsername(String acountOrEmail) throws UsernameNotFoundException {
-//        Staff staff = staffRepository.findByAcountOrEmail(acountOrEmail, acountOrEmail);
-//        if(staff != null){
-//            String username = (staff.getAcount() != null) ? staff.getAcount() : staff.getEmail();
-//            String role = staff.getRole() != null ? staff.getRole().getNameRole() : "USER";
-//
-//            var springStaff = User.withUsername(username)
-//                    .password(this.passwordEncoder().encode(staff.getPassword()))
-//                    .roles(role)
-//                    .build();
-//            return springStaff;
-//        }
-//        return null;
-//    }
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.example.shopgiayonepoly.implement;
+
+import com.example.shopgiayonepoly.entites.Staff;
+import com.example.shopgiayonepoly.repositores.StaffSecurityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaffSecuritiImplement implements UserDetailsService {
+    @Autowired
+    StaffSecurityRepository staffRepository;
+    @Override
+    public UserDetails loadUserByUsername(String acountOrEmail) throws UsernameNotFoundException {
+        Staff staff = staffRepository.findByAcountOrEmail(acountOrEmail, acountOrEmail);
+        if(staff != null){
+            String username = (staff.getAcount() != null) ? staff.getAcount() : staff.getEmail();
+            String role = staff.getRole() != null ? staff.getRole().getNameRole() : "USER";
+
+            var springStaff = User.withUsername(username)
+                    .password(this.passwordEncoder().encode(staff.getPassword()))
+                    .roles(role)
+                    .build();
+            return springStaff;
+        }
+        return null;
+    }
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
