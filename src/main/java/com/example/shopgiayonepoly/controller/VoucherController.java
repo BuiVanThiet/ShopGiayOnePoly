f4ae0d106ff0588d16624dc328d4f31945b3dde0
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-18 21:12:35 +0700

    push,1809,21h13,TuanAnh

--- src/main/java/com/example/shopgiayonepoly/controller/VoucherController.java
+++ src/main/java/com/example/shopgiayonepoly/controller/VoucherController.java
@@ -18,24 +18,31 @@ import org.springframework.web.bind.annotation.*;
 public class VoucherController {
     @Autowired
     private VoucherService voucherService;
-    @GetMapping("/page")
+    @GetMapping("/list")
     public String getListVoucherByPage(Model model, @RequestParam(name = "pageNumber", defaultValue = "1") Integer pageNumber) {
         int pageSize = 5;
-        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
         Page<Voucher> pageVoucher = voucherService.getAllVoucherByPage(pageable);
-        model.addAttribute("pageVoucher", pageVoucher);
+        model.addAttribute("pageVoucher",pageVoucher);
+        model.addAttribute("voucher", new VoucherRequest());
         return "voucher/index";
     }
 
+
     @PostMapping("/create")
-    public String createNewVoucher(Model model,@Valid VoucherRequest voucherRequest, BindingResult result){
-        if(result.hasErrors()){
-            return "";
+    public String createNewVoucher(@Valid @ModelAttribute("voucher") VoucherRequest voucherRequest, BindingResult result, Model model, @RequestParam(name = "pageNumber", defaultValue = "1") Integer pageNumber) {
+        if (result.hasErrors()) {
+            int pageSize = 5;
+            Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+            Page<Voucher> pageVoucher = voucherService.getAllVoucherByPage(pageable);
+            model.addAttribute("pageVoucher", pageVoucher);
+            return "voucher/index";
         }
         voucherService.createNewVoucher(voucherRequest);
-        model.addAttribute("mes","Add new voucher successfully with ID: "+voucherRequest.getId());
-        return "redirect:/voucher/page";
+        model.addAttribute("mes", "Add new voucher successfully with ID: " + voucherRequest.getId());
+        return "redirect:/voucher/list";
     }
+
     @PostMapping("/update")
     public String updateVoucher(Model model,@Valid VoucherRequest voucherRequest, BindingResult result){
         if(result.hasErrors()){
@@ -43,12 +50,12 @@ public class VoucherController {
         }
         voucherService.updateVoucher(voucherRequest);
         model.addAttribute("mes","Update voucher successfully with ID: "+voucherRequest.getId());
-        return "redirect:/voucher/page";
+        return "redirect:/voucher/list";
     }
     @GetMapping("/delete/{id}")
     public String deleteVoucher(Model model, @PathVariable("id")Integer id){
         voucherService.deleteVoucher(id);
-        model.addAttribute("mes","Delete voucher successfully with ID: "+id);
-        return "redirect:/voucher/page";
+        model.addAttribute("mes","Xóa thành công phiếu giảm giá với ID là: "+id);
+        return "redirect:/voucher/list";
     }
 }
