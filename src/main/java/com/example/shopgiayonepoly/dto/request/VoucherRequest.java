4ad0a734059a8e9588670f9f5d25aa4297d973b6
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-19 21:14:27 +0700

    push,1909,21h15,TuanAnh

--- src/main/java/com/example/shopgiayonepoly/dto/request/VoucherRequest.java
+++ src/main/java/com/example/shopgiayonepoly/dto/request/VoucherRequest.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -33,6 +34,8 @@ public class VoucherRequest extends BaseDTO {
     private LocalDate endDate;
     @NotBlank
     private String describe;
+    @NotNull
+    private Integer quantity;
 
 
 }
