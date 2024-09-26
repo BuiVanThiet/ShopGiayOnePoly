9ad448157883d02044ad72e8dcd55eeadf887440
AnhNT-D421 <ntanh280504@gmail.com>
2024-09-26 21:54:00 +0700

    pull,2609,21h53,TuanAnh

--- src/main/resources/static/js/voucher/home.js
+++ src/main/resources/static/js/voucher/home.js
@@ -1,46 +1,46 @@
-function changeTextTypeVoucher() {
-    var selectElement = document.getElementById("discountType");
-    var selectedValue = selectElement.options[selectElement.selectedIndex].value;
+    function changeTextTypeVoucher() {
+        var selectElement = document.getElementById("discountType");
+        var selectedValue = selectElement.options[selectElement.selectedIndex].value;
 
-    var valueReduce = "";
+        var valueReduce = "";
 
-    if (selectedValue === "1") {
-        valueReduce = "%";
-    } else {
-        valueReduce = "₫";
+        if (selectedValue === "1") {
+            valueReduce = "%";
+        } else {
+            valueReduce = "₫";
+        }
+        document.getElementById("discountText").innerText = valueReduce;
     }
-    document.getElementById("discountText").innerText = valueReduce;
-}
 
-window.onload = function () {
-    var discountTypeSelect = document.getElementById("discountType");
-    if (discountTypeSelect) {
-        discountTypeSelect.addEventListener("change", changeTextTypeVoucher);
-        changeTextTypeVoucher();
+    window.onload = function () {
+        var discountTypeSelect = document.getElementById("discountType");
+        if (discountTypeSelect) {
+                discountTypeSelect.addEventListener("change", changeTextTypeVoucher);
+            changeTextTypeVoucher();
+        }
+    };
+
+    function changeTable(tableNumber) {
+        var tableVoucher = document.getElementById("table-voucher");
+        var tableVoucherDelete = document.getElementById("table-voucher-delete");
+
+        if (tableNumber === 1) {
+            tableVoucher.style.display = "block";
+            tableVoucherDelete.style.display = "none";
+            document.getElementById("button1").classList.add("btn-primary");
+            document.getElementById("button1").classList.remove("btn-secondary");
+            document.getElementById("button2").classList.add("btn-secondary");
+            document.getElementById("button2").classList.remove("btn-primary");
+        } else if (tableNumber === 2) {
+            tableVoucher.style.display = "none";
+            tableVoucherDelete.style.display = "block";
+            document.getElementById("button2").classList.add("btn-primary");
+            document.getElementById("button2").classList.remove("btn-secondary");
+            document.getElementById("button1").classList.add("btn-secondary");
+            document.getElementById("button1").classList.remove("btn-primary");
+        }
     }
-};
 
-function changeTable(tableNumber) {
-    var tableVoucher = document.getElementById("table-voucher");
-    var tableVoucherDelete = document.getElementById("table-voucher-delete");
-
-    if (tableNumber === 1) {
-        tableVoucher.style.display = "block";
-        tableVoucherDelete.style.display = "none";
-        document.getElementById("button1").classList.add("btn-primary");
-        document.getElementById("button1").classList.remove("btn-secondary");
-        document.getElementById("button2").classList.add("btn-secondary");
-        document.getElementById("button2").classList.remove("btn-primary");
-    } else if (tableNumber === 2) {
-        tableVoucher.style.display = "none";
-        tableVoucherDelete.style.display = "block";
-        document.getElementById("button2").classList.add("btn-primary");
-        document.getElementById("button2").classList.remove("btn-secondary");
-        document.getElementById("button1").classList.add("btn-secondary");
-        document.getElementById("button1").classList.remove("btn-primary");
-    }
-}
-
-window.onload = function () {
-    changeTable(1);
-};
+    window.onload = function () {
+        changeTable(1);
+    };
