08b8cd7ed6a25b6d23b836e495945c3c60d1cd3f
DoThiAnhNgoc <anhngoc06120@gmail.com>
2024-10-12 16:49:56 +0700

    AnhNgoc,1210,16g50,push

--- src/main/java/com/example/shopgiayonepoly/implement/CustomerImplement.java
+++ src/main/java/com/example/shopgiayonepoly/implement/CustomerImplement.java
@@ -1,20 +1,30 @@
 package com.example.shopgiayonepoly.implement;
 
+import com.cloudinary.Cloudinary;
 import com.example.shopgiayonepoly.dto.response.CustomerResponse;
 import com.example.shopgiayonepoly.entites.Customer;
+import com.example.shopgiayonepoly.entites.Staff;
 import com.example.shopgiayonepoly.repositores.CustomerRepository;
 import com.example.shopgiayonepoly.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerImplement implements CustomerService {
+    private final Cloudinary cloudinary;
+
     @Autowired
     CustomerRepository customerRepository;
 
@@ -71,5 +81,19 @@ public class CustomerImplement implements CustomerService {
     @Override
     public void deleteCustomer(Integer id) {
         customerRepository.deleteBySetStatus(id);
+    }
+
+    @Override
+    public String uploadFile(MultipartFile multipartFile, Integer idCustomer) throws IOException {
+        String nameImage = UUID.randomUUID().toString();
+        Customer customer = this.customerRepository.findById(idCustomer).orElse(null);
+        customer.setImage(nameImage);
+        this.customerRepository.save(customer);
+        // Đẩy file lên Cloudinary với tên file gốc
+        return cloudinary.uploader()
+                .upload(multipartFile.getBytes(),
+                        Map.of("public_id", nameImage)) // Đặt tên file khi upload
+                .get("url")
+                .toString();
     }
 }
