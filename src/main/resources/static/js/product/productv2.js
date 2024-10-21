882f68611dffaba7005d437997ddf3dcc9bc15cc
quanggvinh <vinhdaik2004@gmail.com>
2024-10-21 22:04:48 +0700

    22h05,2110 vinh push code

--- src/main/resources/static/js/product/productv2.js
+++ src/main/resources/static/js/product/productv2.js
@@ -1,61 +1,60 @@
-function toggleDropdown(event, icon) {
+function toggleDropdownProduct(event, icon) {
     var menu = icon.nextElementSibling;
 
     // Kiểm tra và thay đổi trạng thái hiển thị menu
-    menu.classList.toggle('show');
+    menu.classList.toggle('show-product');
 
     // Ngăn chặn sự kiện click lan ra ngoài
     event.stopPropagation();
 }
 
 // Đóng menu khi hover ra khỏi khu vực
-document.querySelectorAll('.dropdown').forEach(function(dropdown) {
+document.querySelectorAll('.dropdown-product').forEach(function(dropdown) {
     dropdown.addEventListener('mouseleave', function() {
-        var menu = this.querySelector('.dropdown-menu');
-        if (menu.classList.contains('show')) {
-            menu.classList.remove('show');
+        var menu = this.querySelector('.dropdown-menu-product');
+        if (menu.classList.contains('show-product')) {
+            menu.classList.remove('show-product');
         }
     });
 });
 
-
 // Đóng menu khi click ra ngoài khu vực menu
 window.onclick = function(event) {
-    if (!event.target.matches('.fa-ellipsis-v')) {
-        var dropdowns = document.getElementsByClassName("dropdown-menu");
+    if (!event.target.matches('.fa-ellipsis-v-product')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu-product");
         for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('show-product')) {
+                openDropdown.classList.remove('show-product');
             }
         }
     }
 }
-function showGridView() {
-    document.querySelector('.form-group').style.display = 'flex';
-    document.querySelector('.table').style.display = 'none';
-    document.querySelector('.fa-th').style.color = 'blue';
-    document.querySelector('.fa-list').style.color = 'black';
+function showGridViewProduct() {
+    document.querySelector('.form-group-product').style.display = 'flex';
+    document.querySelector('.table-product').style.display = 'none';
+    document.querySelector('.fa-th-product').style.color = 'blue';
+    document.querySelector('.fa-list-product').style.color = 'black';
 }
 
-function showListView() {
-    document.querySelector('.form-group').style.display = 'none';
-    document.querySelector('.table').style.display = 'table';
-    document.querySelector('.fa-th').style.color = 'black';
-    document.querySelector('.fa-list').style.color = 'blue';
+function showListViewProduct() {
+    document.querySelector('.form-group-product').style.display = 'none';
+    document.querySelector('.table-product').style.display = 'table';
+    document.querySelector('.fa-th-product').style.color = 'black';
+    document.querySelector('.fa-list-product').style.color = 'blue';
 }
 // Hàm chọn tất cả checkbox
-function toggleSelectAll(selectAllCheckbox) {
-    const checkboxes = document.querySelectorAll('.select-row');
+function toggleSelectAllProduct(selectAllCheckbox) {
+    const checkboxes = document.querySelectorAll('.select-row-product');
     checkboxes.forEach((checkbox) => {
         checkbox.checked = selectAllCheckbox.checked;
     });
 }
 
 // Hàm cập nhật trạng thái checkbox "chọn tất cả" nếu tất cả các checkbox hàng đều được chọn
-document.querySelectorAll('.select-row').forEach((checkbox) => {
+document.querySelectorAll('.select-row-product').forEach((checkbox) => {
     checkbox.addEventListener('change', function() {
-        const allChecked = document.querySelectorAll('.select-row:checked').length === document.querySelectorAll('.select-row').length;
-        document.getElementById('select-all').checked = allChecked;
+        const allChecked = document.querySelectorAll('.select-row-product:checked').length === document.querySelectorAll('.select-row-product').length;
+        document.getElementById('select-all-product').checked = allChecked;
     });
 });
