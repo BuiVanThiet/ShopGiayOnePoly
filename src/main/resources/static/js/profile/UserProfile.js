c62944669a10b37ac4446c933a66ced3400076f8
HungPH33755 <nguyentronghung20122004@gmail.com>
2024-10-21 22:03:42 +0700

    2110 22h03 Push Hung

--- src/main/resources/static/js/profile/UserProfile.js
+++ src/main/resources/static/js/profile/UserProfile.js
@@ -50,6 +50,15 @@ document.getElementById('updateProfileForm').addEventListener('submit', function
         })
         .catch(error => console.error('Error:', error));
 });
+
+document.getElementById('file-upload').onchange = function (event) {
+    const [file] = event.target.files; // Lấy tệp đầu tiên từ danh sách tệp đã chọn
+    if (file) {
+        const avatarPreview = document.getElementById('avatar-preview');
+        avatarPreview.src = URL.createObjectURL(file); // Tạo URL tạm thời và gán cho src của img
+    }
+};
+
 //hiển thị ảnh trên web
 // document.getElementById('file-upload').addEventListener('change', function(event) {
 //     const file = event.target.files[0];
@@ -62,46 +71,6 @@ document.getElementById('updateProfileForm').addEventListener('submit', function
 //         reader.readAsDataURL(file);
 //     }
 // });
-document.getElementById('file-upload').onchange = function (event) {
-    const [file] = event.target.files; // Lấy tệp đầu tiên từ danh sách tệp đã chọn
-    if (file) {
-        const avatarPreview = document.getElementById('avatar-preview');
-        avatarPreview.src = URL.createObjectURL(file); // Tạo URL tạm thời và gán cho src của img
-    }
-};
-
-document.getElementById('updateProfileForm').addEventListener('submit', function (event) {
-    var isValid = true;
-
-    // Xóa các thông báo lỗi trước đó
-    document.getElementById('fullname-error').innerText = '';
-    document.getElementById('email-error').innerText = '';
-    document.getElementById('sdt-error').innerText = '';
-
-    var fullName = document.getElementById('fullname').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var numberPhone = document.getElementById('sdt').value.trim();
-
-    if (fullName === '') {
-        document.getElementById('fullname-error').innerText = 'Vui lòng nhập họ và tên.';
-        isValid = false;
-    }
-
-    if (email === '') {
-        document.getElementById('email-error').innerText = 'Vui lòng nhập email.';
-        isValid = false;
-    }
-
-    if (numberPhone === '') {
-        document.getElementById('sdt-error').innerText = 'Vui lòng nhập số điện thoại.';
-        isValid = false;
-    }
-
-    // Nếu không hợp lệ, ngăn không cho form submit
-    if (!isValid) {
-        event.preventDefault();
-    }
-});
 
 //
 // // // Hàm để kiểm tra năm nhuận
