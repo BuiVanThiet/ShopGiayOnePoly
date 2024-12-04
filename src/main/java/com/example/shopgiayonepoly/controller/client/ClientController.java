bf98ea2f9f0bdd08df6f3d4c946d377963c6191f
HungPH33755 <nguyentronghung20122004@gmail.com>
2024-12-04 13:32:46 +0700

    0412 13h32 push hung

--- src/main/java/com/example/shopgiayonepoly/controller/client/ClientController.java
+++ src/main/java/com/example/shopgiayonepoly/controller/client/ClientController.java
@@ -657,7 +657,6 @@ public class ClientController extends BaseBill {
         model.addAttribute("errorMessage", "");
 //        return "login/loginClient";
         return "client/homepage";
-//        return "redirect:/onepoly/home";  để khi đăng xuất thì hiển thị đăng nhập
     }
 
     @PostMapping("/login")
